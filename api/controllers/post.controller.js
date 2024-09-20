d74234c742b8648beb49342141c58d29514848d2
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-21 03:28:49 +0400

    Add Create Comment Api Route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -43,31 +43,67 @@ export const createPost = async (req, res, next) => {
 };
 
 // 2-Function to Delete Post:
-export const deletePost = async(req,res,next)=>{
+export const deletePost = async (req, res, next) => {
   try {
-    const {id} = req.params;
+    const { id } = req.params;
     // Find The Post:
-    const post = await Post.findById(id)
-    if(!post){
+    const post = await Post.findById(id);
+    if (!post) {
       return next(handleErrors(404, "Post Not Found!"));
     }
-// check if you can delete the post or not || if you are the owner of the post:
-if(post.user.toString() !== req.user._id.toString()){
-  return next(handleErrors(401, "You are not authorized to delete this post!"));
-}
+    // check if you can delete the post or not || if you are the owner of the post:
+    if (post.user.toString() !== req.user._id.toString()) {
+      return next(
+        handleErrors(401, "You are not authorized to delete this post!")
+      );
+    }
     // delete image from cloudinary:
-    if(post.image){
-      const imageID = post.image.split('/').pop().split('.')[0]
-      await cloudinary.uploader.destroy(imageID)
+    if (post.image) {
+      const imageID = post.image.split("/").pop().split(".")[0];
+      await cloudinary.uploader.destroy(imageID);
     }
 
     // delete the post:
-    await Post.findByIdAndDelete(id)
-    return res.status(200).json({message:"Post Deleted Successfully"})
-    
+    await Post.findByIdAndDelete(id);
+    return res.status(200).json({ message: "Post Deleted Successfully" });
   } catch (error) {
     console.log("Error In Creating Delete Post Api Route", error.message);
     next(error);
-    
   }
-}
+};
+
+// 3-Function to Comment On Post:
+export const commentOnPost = async (req, res, next) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+
+    // Find The Post:
+    const post = await Post.findById(postId);
+    if (!post) {
+      return next(handleErrors(404, "Post Not Found!"));
+    }
+
+    if (!text) {
+      return next(handleErrors(400, "Text Field Is Required!"));
+    }
+
+    // create a comment:
+    const newComment = {
+      user: userId,
+      text,
+    };
+
+    // update the comment array of the post:
+    post.comments.push(newComment);
+
+    // save the post:
+    await post.save();
+
+    res.status(200).json(post);
+  } catch (error) {
+    console.log("Error In Creating Comment On Post Api Route", error.message);
+    next(error);
+  }
+};
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleWare/verifyToken.js";
-import { createPost, deletePost } from "../controllers/post.controller.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/createpost", protectRoute, createPost);
 router.delete("/deletepost/:id", protectRoute, deletePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 
 export default router;
