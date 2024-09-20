de926177e998cc96d1c9a375ee3d5c0474bb3859
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-21 03:59:24 +0400

    Creat Like&Unlike Api Route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,6 +2,7 @@ import Post from "../models/post.model.js";
 import { handleErrors } from "../utils/error.js";
 import User from "../models/user.model.js";
 import cloudinary from "../config/cloudinary.js";
+import Notification from "../models/notification.model.js";
 
 // 1-Function To Create Post:
 export const createPost = async (req, res, next) => {
@@ -9,14 +10,11 @@ export const createPost = async (req, res, next) => {
     const { title } = req.body;
     let { image } = req.body;
     const userId = req.user._id.toString();
-
     // Find The User:
     const user = await User.findById(userId);
-
     if (!user) {
       return next(handleErrors(404, "User Not Found!"));
     }
-
     if ((!title && !image) || (title === "" && image === "")) {
       return next(handleErrors(400, "Please Enter All Required Fields!"));
     }
@@ -25,14 +23,12 @@ export const createPost = async (req, res, next) => {
       const uploadedPostImage = await cloudinary.uploader.upload(image);
       image = uploadedPostImage.secure_url;
     }
-
     // create a new post:
     const newPost = new Post({
       user: userId,
       title,
       image,
     });
-
     // save the post:
     await newPost.save();
     res.status(201).json(newPost);
@@ -62,7 +58,6 @@ export const deletePost = async (req, res, next) => {
       const imageID = post.image.split("/").pop().split(".")[0];
       await cloudinary.uploader.destroy(imageID);
     }
-
     // delete the post:
     await Post.findByIdAndDelete(id);
     return res.status(200).json({ message: "Post Deleted Successfully" });
@@ -78,32 +73,66 @@ export const commentOnPost = async (req, res, next) => {
     const { text } = req.body;
     const postId = req.params.id;
     const userId = req.user._id;
-
     // Find The Post:
     const post = await Post.findById(postId);
     if (!post) {
       return next(handleErrors(404, "Post Not Found!"));
     }
-
     if (!text) {
       return next(handleErrors(400, "Text Field Is Required!"));
     }
-
     // create a comment:
     const newComment = {
       user: userId,
       text,
     };
-
     // update the comment array of the post:
     post.comments.push(newComment);
-
     // save the post:
     await post.save();
-
     res.status(200).json(post);
   } catch (error) {
     console.log("Error In Creating Comment On Post Api Route", error.message);
+    next(error);
+  }
+};
+
+// 4-Function to Like Or Unlike Post:
+export const likeUnlikePost = async (req, res, next) => {
+  try {
+    const { id: postId } = req.params;
+    const userId = req.user._id;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return next(handleErrors(404, "Post not Found!"));
+    }
+    const userLikedPost = post.likes.includes(userId);
+    if (userLikedPost) {
+      // unlike the post:
+      await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
+      await User.updateOne({ _id: userId }, { $pull: { likesPost: postId } });
+      res.status(200).json({ message: "Post Unliked Successfully" });
+    } else {
+      // like the post:
+      // await Post.updateOne({_id:postId},{$push:{likes:userId}}) // first way to like on the post
+      post.likes.push(userId); // second way to like on the post
+      await User.updateOne({ _id: userId }, { $push: { likesPost: postId } });
+      await post.save();
+      // create notification:
+      const notification = new Notification({
+        from: userId,
+        to: post.user,
+        type: "like",
+      });
+      // save the notification:
+      await notification.save();
+      res.status(200).json({ message: "Post Liked Successfully" });
+    }
+  } catch (error) {
+    console.log(
+      "Error In Creating Like Or Unlike Post Api Route",
+      error.message
+    );
     next(error);
   }
 };
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -4,6 +4,7 @@ import {
   commentOnPost,
   createPost,
   deletePost,
+  likeUnlikePost,
 } from "../controllers/post.controller.js";
 
 const router = express.Router();
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/createpost", protectRoute, createPost);
 router.delete("/deletepost/:id", protectRoute, deletePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
 
 export default router;
