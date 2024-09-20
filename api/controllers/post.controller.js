09a4c8a46b0e43ea0c69612fac05c18dc7fe8333
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-21 02:56:24 +0400

    2-Create Delete Post Api Route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -41,3 +41,33 @@ export const createPost = async (req, res, next) => {
     next(error);
   }
 };
+
+// 2-Function to Delete Post:
+export const deletePost = async(req,res,next)=>{
+  try {
+    const {id} = req.params;
+    // Find The Post:
+    const post = await Post.findById(id)
+    if(!post){
+      return next(handleErrors(404, "Post Not Found!"));
+    }
+// check if you can delete the post or not || if you are the owner of the post:
+if(post.user.toString() !== req.user._id.toString()){
+  return next(handleErrors(401, "You are not authorized to delete this post!"));
+}
+    // delete image from cloudinary:
+    if(post.image){
+      const imageID = post.image.split('/').pop().split('.')[0]
+      await cloudinary.uploader.destroy(imageID)
+    }
+
+    // delete the post:
+    await Post.findByIdAndDelete(id)
+    return res.status(200).json({message:"Post Deleted Successfully"})
+    
+  } catch (error) {
+    console.log("Error In Creating Delete Post Api Route", error.message);
+    next(error);
+    
+  }
+}
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protectRoute } from "../middleWare/verifyToken.js";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, deletePost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
 router.post("/createpost", protectRoute, createPost);
+router.delete("/deletepost/:id", protectRoute, deletePost);
 
 export default router;
