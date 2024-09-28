79c27330f98df36cf9af739f1263890252e07e1c
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-09-28 20:00:22 +0400

    Prepare The App For Deployment

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import path from "path";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
@@ -10,13 +11,23 @@ import notificationRoutes from "./routes/notification.route.js";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(express.json({limit:'10mb'}));  // limit shouldn't be too high to prevent DOS Attacks//
+const __dirname = path.resolve();
+app.use(express.json({ limit: "10mb" })); // limit shouldn't be too high to prevent DOS Attacks//
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
+
+// Serve static files from the build folder:
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 // Listen to the port:
 app.listen(PORT, () => {
   connectDB();
